eeafbadcc41dca40e59690f7a7fbb5324549cef3
Braismp007 <90138396+Braismp007@users.noreply.github.com>
2024-02-04 18:10:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Revisando TITAN","Jugando SummerCraft","Preparando póximo evento"];
 
 
 let currentIndex = 0;
